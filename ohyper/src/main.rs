3af98b00b8da59139e7ef8fb3e30ca59730941b0
Gege-Wang <2891067867@qq.com>
2024-09-04 16:17:18 +0800

    add: log and uart

--- ohyper/src/main.rs
+++ ohyper/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+use core::fmt::Write;
+
+#[macro_use]
+extern crate log;
+
+mod lang_items;
+mod uart;
+mod uart16550;
+#[macro_use]
+mod logging;
+
+const HELLO: &'static str = r"
+    OOOOO   HH   HH YYY   YYY PPPPPP  EEEEEEE RRRRRR
+   OO   OO  HH   HH  YYY YYY  PP   PP EE      RR   RR
+   OO   OO  HHHHHHH   YYY     PPPPPP  EEEEE   RRRRRR
+   OO   OO  HH   HH   YYY     PP      EE      RR  RR
+    OOOOO   HH   HH   YYY     PP      EEEEEEE RR   RR
+";
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("{}", HELLO);
+    println!(
+        "\
+        arch = {}\n\
+        build_mode = {}\n\
+        log_level = {}\n\
+        ",
+        option_env!("ARCH").unwrap_or(""),
+        option_env!("MODE").unwrap_or(""),
+        option_env!("LOG").unwrap_or(""),
+    );
+    logging::init();
+    info!("Logging initialized");
+    loop{}
 }
---
+++ ohyper/src/uart.rs
@@ -0,0 +1,49 @@
+use x86_64::instructions::port::Port;
+use core::fmt::Write;
+
+pub struct SerialPort {
+    port: u16,
+}
+
+impl SerialPort {
+    pub fn new(port: u16) -> Self {
+        let mut port = SerialPort { port };
+
+        port.write_register(1, 0x00); // disable interrupts
+        port.write_register(3, 0x80); // enable DLAB
+        port.write_register(0, 0x01); // set baud rate
+        port.write_register(1, 0x00); // set baud rate
+        port.write_register(3, 0x03); // 8 bits data, data checksum 
+
+        port
+    }
+
+    fn write_register(&mut self, reg: u16, value: u8) {
+        unsafe {
+            Port::new(self.port + reg).write(value);
+        }
+    }
+
+    // send a data from port
+    pub fn send(&mut self, data: u8) {
+        while self.is_transmit_empty() == false {}
+        self.write_register(0, data);
+    }
+
+    fn is_transmit_empty(&mut self) -> bool {
+        unsafe {
+            let lsb: u8 = Port::new(self.port + 5).read();
+            lsb & 0x20 != 0
+        }
+
+    }
+}
+
+impl Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        for byte in s.bytes() {
+            self.send(byte);
+        }
+        Ok(())
+    }
+}
